9b028431c90d5d78ef9f189976ec75a7afae3622
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-01-30 15:56:39 +0100

    Add PublicationResource TCA

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,27 @@
+<?php
+defined('TYPO3') or die();
+declare(strict_types=1);
+
+# This file is part of the extension CHF Pub for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+/**
+ * Rules to map object inheritance to TCA tables
+ * 
+ * List of inherited object models and how they relate to TCA tables and
+ * values of the property specified as 'type' in the TCA config. Simpler
+ * objects based on tables of the same name and without multiple types
+ * do not need to be listed here since Extbase maps them automatically.
+ * For more information on the persistence of Extbase models see
+ * https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/Extbase/Reference/Domain/Persistence.html.
+ */
+return [
+    Digicademy\CHFBase\Domain\Model\AbstractResource::class => [
+        'subclasses' => [
+            'publicationResource' => Digicademy\CHFPub\Domain\Model\PublicationResource::class,
+        ]
+    ],
+];
---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_resource.php
@@ -0,0 +1,85 @@
+<?php
+defined('TYPO3') or die();
+
+# This file is part of the extension CHF Pub for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+/**
+ * PublicationResource and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add select item 'publicationResource'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem('tx_chfbase_domain_model_resource', 'type',
+    [
+        'label' => 'LLL:EXT:chf_pub/Resources/Private/Language/locallang.xlf:object.publicationResource.type.publicationResource',
+        'value' => 'publicationResource',
+    ]
+);
+
+// Add columns 'allVolumes' and 'allEssays'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_resource',
+    [
+        'allVolumes' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_pub/Resources/Private/Language/locallang.xlf:object.publicationResource.allVolumes',
+            'description' => 'LLL:EXT:chf_pub/Resources/Private/Language/locallang.xlf:object.publicationResource.allVolumes.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfpub_domain_model_volume',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+        'allEssays' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_pub/Resources/Private/Language/locallang.xlf:object.publicationResource.allEssays',
+            'description' => 'LLL:EXT:chf_pub/Resources/Private/Language/locallang.xlf:object.publicationResource.allEssays.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfpub_domain_model_essay',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+    ]
+);
+
+// Add type 'publicationResource' and its 'showitem' list
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+   'tx_chfbase_domain_model_resource',
+   'hiddenUuid,typeUri,titleLangCode,glossary,description,sameAs,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.editorial,authorshipRelation,licenceRelation,publicationDateRevisionNumberRevisionDate,editorialNote,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.content,allAgents,allFileCollections,allLocations,allPeriods,allRelations,allTags,allVolumes,allEssays,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.import,importOrigin,importState,'
+   'publicationResource'
+);

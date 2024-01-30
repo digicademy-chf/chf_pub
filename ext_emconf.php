de255fc9e57bb57e18f3b8d612f1a76f9f645691
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-01-30 13:23:00 +0100

    TYPO3 13 upgrade

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'        => '0.6.0',
     'constraints'    => [
         'depends'   => [
-            'typo3' => '12.0.0-12.99.99'
+            'typo3' => '13.0.0-13.99.99'
         ],
         'conflicts' => [
         ],

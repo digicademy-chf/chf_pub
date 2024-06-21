0ba08a813bf1b6c424f6ebcb0f8b482b78684318
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-22 01:19:59 +0200

    Add MM table fields and unify floor plan fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -13,3 +13,15 @@ CREATE TABLE tx_chfpub_domain_model_volume (
 CREATE TABLE tx_chfbase_domain_model_relation (
     position varchar(255) DEFAULT '' NOT NULL
 );
+
+# Remove when forge.typo3.org/issues/98322 is fixed to auto-generate these fields
+
+CREATE TABLE tx_chfpub_domain_model_essay_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chfpub_domain_model_volume_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);

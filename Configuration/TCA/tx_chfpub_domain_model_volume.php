06b53e02d777dcd711927df0a799e0c53d13378f
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2025-02-11 14:54:47 +0100

    Add IRIs to TCA

--- Configuration/TCA/tx_chfpub_domain_model_volume.php
+++ Configuration/TCA/tx_chfpub_domain_model_volume.php
@@ -35,7 +35,7 @@ return [
         'transOrigPointerField'    => 'l10n_parent',
         'transOrigDiffSourceField' => 'l10n_diffsource',
         'translationSource'        => 'l10n_source',
-        'searchFields'             => 'title,uuid,publication_date,revision_date,revision_number,editorial_note,import_origin,import',
+        'searchFields'             => 'title,iri,uuid,publication_date,revision_date,revision_number,editorial_note,import_origin,import',
         'enablecolumns'            => [
             'disabled' => 'hidden',
             'starttime' => 'starttime',
@@ -415,6 +415,32 @@ return [
                     'label' => 'asc',
                 ],
                 'required' => true,
+            ],
+        ],
+        'iri' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.iri',
+            'description' => 'LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.iri.description',
+            'config' => [
+                'type' => 'slug',
+                'size' => 40,
+                'appearance' => [
+                    'prefix' => 'Digicademy\CHFBase\UserFunctions\FormEngine\SlugPrefix->getPrefix',
+                ],
+                'prependSlash' => false,
+                'generatorOptions' => [
+                    'fields' => [
+                        'uid',
+                    ],
+                    'fieldSeparator' => '/',
+                    'prefixParentPageSlug' => false,
+                    'replacements' => [
+                        '/' => '',
+                    ],
+                ],
+                'eval' => 'uniqueInSite',
+                'fallbackCharacter' => '',
             ],
         ],
         'uuid' => [
